660d6ba36317725a349a4dbce5b259ac73fd9664
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-18 17:06:03 -0600

    Update model relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
+
+  has_many :invoices
+  has_many :items
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 end
