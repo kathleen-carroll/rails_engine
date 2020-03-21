7f0886fe132efee2075a18cab9d90eba168d9a03
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-21 11:07:13 -0600

    Finish merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :create, :update, :destroy]
+      # , :show
       resources :items
+
+      resources :merchants, only: [:show] do
+        resources :items, only: [:index], controller: 'merchant_items'
+      end
+      # get '/merchants/:merchant_id/items', to: 'merchant_items#index'
     end
   end
 end
